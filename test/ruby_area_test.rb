a5a0233851354398e7fb0c0cc895d1b744f5ff86
Ziyan Junaideen <jdeen-solutions@outlook.com>
2015-03-08 09:15:51 +0530

    Test to check perch and acre extraction

--- test/ruby_area_test.rb
+++ test/ruby_area_test.rb
@@ -4,4 +4,12 @@ class RubyAreaTest < ActiveSupport::TestCase
   test "truth" do
     assert_kind_of Module, RubyArea
   end
+
+  def test_to_area_extracts_acres_in_number
+    assert_equal 180.acre, 1
+  end
+
+  def test_to_area_extract_perch_in_number
+    assert_equal 180.perch, 20
+  end
 end
